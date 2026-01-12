72177150ba75ae7d0951bae36d0318856017d44c
Julian Jaramillo <thatkidjuju18@icloud.com>
2026-01-12 10:42:33 -0800

    Initial Commit

---
+++ WordGarden App/WordGarden_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_AppApp.swift
+//  WordGarden App
+//
+//  Created by JARAMILLO, JULIAN on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordGarden AppTests/WordGarden_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_AppTests.swift
+//  WordGarden AppTests
+//
+//  Created by JARAMILLO, JULIAN on 1/12/26.
+//
+
+import Testing
+@testable import WordGarden_App
+
+struct WordGarden_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
